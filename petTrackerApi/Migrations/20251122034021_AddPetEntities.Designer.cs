223182cc9ce4aa0c07aa8f0d7a167344a80700da
LuisAngel98 <luis37813@gmail.com>
2025-11-21 22:41:33 -0500

    feat(core,db): add initial entities (Especie, FotosMascota, Mascota, Raza) and update DbContext with new migration

--- petTrackerApi/Migrations/20251122034021_AddPetEntities.Designer.cs
+++ petTrackerApi/Migrations/20251122034021_AddPetEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using petTrackerApi.Data;
 
@@ -11,9 +12,11 @@ using petTrackerApi.Data;
 namespace petTrackerApi.Migrations
 {
     [DbContext(typeof(DBContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20251122034021_AddPetEntities")]
+    partial class AddPetEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
