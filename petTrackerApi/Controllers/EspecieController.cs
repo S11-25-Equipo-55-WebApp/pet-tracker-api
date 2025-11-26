73ddc00783c40ae6f16a13d7f79647b85f5ae205
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-11-26 00:53:45 -0300

    get por id especies

--- petTrackerApi/Controllers/EspecieController.cs
+++ petTrackerApi/Controllers/EspecieController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using petTrackerApi.DTO;
 using petTrackerApi.Services;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,9 +27,11 @@ namespace petTrackerApi.Controllers
 
         // GET api/<EspecieController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<EspecieDTO>> GetById(int id)
         {
-            return "value";
+            var result = await _service.GetById(id);
+            if (result == null) return NotFound();
+            return Ok(result);
         }
 
         // POST api/<EspecieController>
