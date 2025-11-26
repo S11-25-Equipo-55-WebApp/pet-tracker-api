d3edb49759d77e2bfd2ed22018c484b5b1be7484
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-11-26 09:27:16 -0300

    repositorio de razas

---
+++ petTrackerApi/Repository/RazaRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using petTrackerApi.Data;
+using petTrackerApi.Model;
+
+namespace petTrackerApi.Repository
+{
+    public class RazaRepository : IRazaRepository
+    {
+        private readonly DBContext _db;
+
+        public RazaRepository(DBContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<Raza>> Get()
+        {
+            return await _db.Razas.ToListAsync();
+        }
+
+        public async Task<Raza> GetById(int id)
+        {
+            return await _db.Razas.FindAsync(id);
+        }
+
+        public Task<Raza> Registro(Raza raza)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Raza> Update(int id, Raza raza)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Raza> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
