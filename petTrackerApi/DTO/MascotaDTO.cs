99651e642abc1e8346967de7e2d943098e2b806f
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-11-26 17:06:06 -0300

    agrego generics para servicios y repositorios

---
+++ petTrackerApi/DTO/MascotaDTO.cs
@@ -0,0 +1,27 @@
+﻿using petTrackerApi.Model;
+
+namespace petTrackerApi.DTO
+{
+    public class MascotaDTO
+    {
+        public int MascotaId { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string Codigo { get; set; } = null!;
+
+        public DateOnly? FechaNacimiento { get; set; }
+
+        public int EspecieId { get; set; }
+
+        public int RazaId { get; set; }
+
+        public int FotoMascotaId { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public DateTime CreadoAt { get; set; }
+
+        public DateTime EditadoAt { get; set; }
+    }
+}
---
+++ petTrackerApi/Services/GenericServices/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace petTrackerApi.Services.GenericServices
+{
+    public interface IGenericService<T>
+    {
+        Task<IEnumerable<T>> Get();
+        Task<T> GetById(int id);
+        Task<T> Create(T dto);
+        Task<T> Update(int id, T dto);
+        Task<T> Delete(int id);
+    }
+}
