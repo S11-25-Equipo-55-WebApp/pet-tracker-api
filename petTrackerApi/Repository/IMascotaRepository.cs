4170ec5baf4f8c00eb9c4c4e362c402effb35b56
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-11-26 10:05:58 -0300

    repositorio de mascota

---
+++ petTrackerApi/Repository/IMascotaRepository.cs
@@ -0,0 +1,13 @@
+﻿using petTrackerApi.Model;
+
+namespace petTrackerApi.Repository
+{
+    public interface IMascotaRepository
+    {
+        Task<IEnumerable<Mascota>> Get();
+        Task<Mascota> GetById(int id);
+        Task<Mascota> Registro(Mascota mascota);
+        Task<Mascota> Update(int id, Mascota mascota);
+        Task<Mascota> Delete(int id);
+    }
+}
---
+++ petTrackerApi/Repository/MascotaRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using petTrackerApi.Data;
+using petTrackerApi.Model;
+
+namespace petTrackerApi.Repository
+{
+    public class MascotaRepository : IMascotaRepository
+    {
+        private readonly DBContext _db;
+
+        public MascotaRepository(DBContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<Mascota>> Get()
+        {
+            return await _db.Mascota.ToListAsync();
+        }
+
+        public async Task<Mascota> GetById(int id)
+        {
+            return await _db.Mascota.FindAsync(id);
+        }
+
+        public async Task<Mascota> Registro(Mascota mascota)
+        {
+            _db.Mascota.Add(mascota);
+            await _db.SaveChangesAsync();
+            return mascota;
+        }
+
+        public async Task<Mascota> Update(int id, Mascota mascota)
+        {
+            var mascotaDB = await _db.Mascota.FindAsync(id);
+            if (mascotaDB == null) return null;
+
+            mascotaDB.Nombre = mascota.Nombre;
+            mascotaDB.FechaNacimiento = mascota.FechaNacimiento;
+            mascotaDB.EspecieId = mascota.EspecieId;
+            mascotaDB.RazaId = mascota.RazaId;
+            mascotaDB.FotoMascotaId = mascota.FotoMascotaId;
+            mascotaDB.EditadoAt = DateTime.Now;
+
+            await _db.SaveChangesAsync();
+            return mascotaDB;
+        }
+        public async Task<Mascota> Delete(int id)
+        {
+            var mascotaDB = await _db.Mascota.FindAsync(id);
+            if (mascotaDB == null) return null;
+
+            _db.Mascota.Remove(mascotaDB);
+            await _db.SaveChangesAsync();
+
+            return mascotaDB;
+        }
+    }
+}
