8ab125f5098d68bc6fa8088ce500628f90726a02
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-11-28 15:51:54 -0300

    repositorio de tipo de desparacitacion

---
+++ petTrackerApi/Repository/TipoDesparacitacionRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using petTrackerApi.Data;
+using petTrackerApi.Model;
+using petTrackerApi.Repository.GenericRepository;
+
+namespace petTrackerApi.Repository
+{
+    public class TipoDesparacitacionRepository : IGenericRepository<TipoDesparacitacion>
+    {
+        private readonly DBContext _db;
+
+        public TipoDesparacitacionRepository(DBContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<TipoDesparacitacion>> Get()
+        {
+            return await _db.TipoDesparacitaciones.ToListAsync();
+        }
+
+        public async Task<TipoDesparacitacion> GetById(int id)
+        {
+            return await _db.TipoDesparacitaciones.FindAsync(id);
+        }
+        public Task<TipoDesparacitacion> Create(TipoDesparacitacion entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TipoDesparacitacion> Update(TipoDesparacitacion entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TipoDesparacitacion> Delete(TipoDesparacitacion entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
