5f0e2b5be3bd4f15183065e7b25041b37515391b
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-11-29 23:53:28 -0300

    vacuna repository

---
+++ petTrackerApi/Repository/IRepository/IVacunaRepository.cs
@@ -0,0 +1,10 @@
+﻿using petTrackerApi.Model;
+using petTrackerApi.Repository.GenericRepository;
+
+namespace petTrackerApi.Repository.IRepository
+{
+    public interface IVacunaRepository : IGenericRepository<Vacuna>
+    {
+        Task<IEnumerable<Vacuna>> GetVacunasByIdMascota(int id);
+    }
+}
