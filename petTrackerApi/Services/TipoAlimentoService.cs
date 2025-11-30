7fadaf28c438d9f4471c0004f454784800bc0573
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-11-30 00:51:40 -0300

    tipoAlimento service

---
+++ petTrackerApi/Services/TipoAlimentoService.cs
@@ -0,0 +1,48 @@
+﻿using petTrackerApi.Data;
+using petTrackerApi.DTO;
+using petTrackerApi.Model;
+using petTrackerApi.Repository.GenericRepository;
+using petTrackerApi.Services.GenericServices;
+
+namespace petTrackerApi.Services
+{
+    public class TipoAlimentoService : IGenericService<TipoAlimentoDTO>
+    {
+        private readonly IGenericRepository<TipoAlimento> _repository;
+
+        public TipoAlimentoService(IGenericRepository<TipoAlimento> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IEnumerable<TipoAlimentoDTO>> Get()
+        {
+            var tipoAlimentos = await _repository.Get();
+            return tipoAlimentos.Select(Mapper.TipoAlimentoMapToDTO);
+        }
+
+        public async Task<TipoAlimentoDTO> GetById(int id)
+        {
+            var encuentra = await _repository.GetById(id);
+            if (encuentra == null) return null;
+            var respuesta = Mapper.TipoAlimentoMapToDTO(encuentra);
+            return respuesta;
+        }
+
+        public Task<TipoAlimentoDTO> Create(TipoAlimentoDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TipoAlimentoDTO> Update(int id, TipoAlimentoDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TipoAlimentoDTO> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
