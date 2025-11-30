3f833ff153c4a57c5adbe162def25561c5f440b7
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-11-30 00:44:01 -0300

    TipoAlimentoDTO y mapper

---
+++ petTrackerApi/DTO/TipoAlimentoDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace petTrackerApi.DTO
+{
+    public class TipoAlimentoDTO
+    {
+        public int TipoAlimentoId { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string Codigo { get; set; } = null!;
+    }
+}
