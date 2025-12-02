291b1261a57408665ea3312578a16e24d54500bc
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-12-02 00:22:27 -0300

    IRepository y repository

---
+++ petTrackerApi/Repository/IRepository/IControlPesoRepository.cs
@@ -0,0 +1,10 @@
+﻿using petTrackerApi.Model;
+using petTrackerApi.Repository.GenericRepository;
+
+namespace petTrackerApi.Repository.IRepository
+{
+    public interface IControlPesoRepository : IGenericRepository<ControlPeso>
+    {
+        Task<IEnumerable<ControlPeso>> GetControlPesoByIdMascota(int id);
+    }
+}
