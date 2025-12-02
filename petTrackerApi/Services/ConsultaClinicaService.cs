faba4395c4689f0a4144acb8b3ea99ad7e693964
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-12-02 01:35:50 -0300

    repository

---
+++ petTrackerApi/Services/ConsultaClinicaService.cs
@@ -0,0 +1,41 @@
+﻿using petTrackerApi.Data;
+using petTrackerApi.DTO;
+using petTrackerApi.Model;
+using petTrackerApi.Repository.GenericRepository;
+using petTrackerApi.Repository.IRepository;
+using petTrackerApi.Services.GenericServices;
+
+namespace petTrackerApi.Services
+{
+    public class ConsultaClinicaService : IGenericService<ConsultaClinicaDTO>
+    {
+        private readonly IGenericRepository<ConsultaClinica> _repository;
+        public ConsultaClinicaService(IGenericRepository<ConsultaClinica> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IEnumerable<ConsultaClinicaDTO>> Get()
+        {
+            var consultasClinicas = await _repository.Get();
+            return consultasClinicas.Select(Mapper.ConsultaClinicaMapToDTO);
+        }
+
+        public Task<ConsultaClinicaDTO> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ConsultaClinicaDTO> Create(ConsultaClinicaDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ConsultaClinicaDTO> Update(int id, ConsultaClinicaDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ConsultaClinicaDTO> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
