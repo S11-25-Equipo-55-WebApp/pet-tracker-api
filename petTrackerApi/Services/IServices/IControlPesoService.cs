06d4fc8884fdd6f530901e92bdfd802a6a1a2730
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-12-02 00:46:53 -0300

    control de peso service

---
+++ petTrackerApi/Services/IServices/IControlPesoService.cs
@@ -0,0 +1,10 @@
+﻿using petTrackerApi.DTO;
+using petTrackerApi.Services.GenericServices;
+
+namespace petTrackerApi.Services.IServices
+{
+    public interface IControlPesoService : IGenericService<ControlPesoDTO>
+    {
+        Task<IEnumerable<ControlPesoDTO>> GetControlPesoByIdMascota(int id);
+    }
+}
