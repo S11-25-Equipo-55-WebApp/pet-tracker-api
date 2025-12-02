27dfd58ccfacd2d3384c3e9d67de0a11ba6105ab
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-12-02 01:48:54 -0300

    service, creo interfaces personalizadas

--- petTrackerApi/Services/ConsultaClinicaService.cs
+++ petTrackerApi/Services/ConsultaClinicaService.cs
@@ -1,16 +1,17 @@
 ﻿using petTrackerApi.Data;
 using petTrackerApi.DTO;
+using petTrackerApi.Helpers;
 using petTrackerApi.Model;
 using petTrackerApi.Repository.GenericRepository;
 using petTrackerApi.Repository.IRepository;
-using petTrackerApi.Services.GenericServices;
+using petTrackerApi.Services.IServices;
 
 namespace petTrackerApi.Services
 {
-    public class ConsultaClinicaService : IGenericService<ConsultaClinicaDTO>
+    public class ConsultaClinicaService : IConsultaClinicaService
     {
-        private readonly IGenericRepository<ConsultaClinica> _repository;
-        public ConsultaClinicaService(IGenericRepository<ConsultaClinica> repository)
+        private readonly IConsultaClinicaRepository _repository;
+        public ConsultaClinicaService(IConsultaClinicaRepository repository)
         {
             _repository = repository;
         }
@@ -20,22 +21,56 @@ namespace petTrackerApi.Services
             return consultasClinicas.Select(Mapper.ConsultaClinicaMapToDTO);
         }
 
-        public Task<ConsultaClinicaDTO> GetById(int id)
+        public async Task<ConsultaClinicaDTO> GetById(int id)
         {
-            throw new NotImplementedException();
+            var encuentra = await _repository.GetById(id);
+            if (encuentra == null) return null;
+            var respuesta = Mapper.ConsultaClinicaMapToDTO(encuentra);
+            return respuesta;
         }
-        public Task<ConsultaClinicaDTO> Create(ConsultaClinicaDTO dto)
+        public async Task<ConsultaClinicaDTO> Create(ConsultaClinicaDTO dtoConsulta)
         {
-            throw new NotImplementedException();
+            var mapConsulta = Mapper.ConsultaClinicaDTOMapToEntity(dtoConsulta);
+            mapConsulta.CreadoAt = DateTime.Now;
+            mapConsulta.EditadoAt = DateTime.Now;
+            mapConsulta.Codigo = CodeGenerator.GenerarCodigoDobleFechaDateTime(mapConsulta.MascotaId, mapConsulta.CreadoAt, mapConsulta.EditadoAt);
+
+            var consultaCreada = await _repository.Create(mapConsulta);
+            var consultaResult = Mapper.ConsultaClinicaMapToDTO(consultaCreada);
+            return consultaResult;
         }
 
-        public Task<ConsultaClinicaDTO> Update(int id, ConsultaClinicaDTO dto)
+        public async Task<ConsultaClinicaDTO> Update(int id, ConsultaClinicaDTO dtoConsulta)
         {
-            throw new NotImplementedException();
+            var consultaDB = await _repository.GetById(id);
+            if (consultaDB == null) return null;
+
+            consultaDB.FechaConsulta = dtoConsulta.FechaConsulta;
+            consultaDB.Motivo = dtoConsulta.Motivo;
+            consultaDB.Diagnostico = dtoConsulta.Diagnostico;
+            consultaDB.Veterinario  = dtoConsulta.Veterinario;
+            consultaDB.Notas = dtoConsulta.Notas;
+            consultaDB.MascotaId = dtoConsulta.MascotaId;
+            consultaDB.EditadoAt = DateTime.Now;
+            consultaDB.Codigo = CodeGenerator.GenerarCodigoDobleFechaDateTime(consultaDB.MascotaId, consultaDB.CreadoAt, consultaDB.EditadoAt);
+
+            var actualizado = await _repository.Update(consultaDB);
+            return actualizado == null ? null : Mapper.ConsultaClinicaMapToDTO(actualizado);
         }
-        public Task<ConsultaClinicaDTO> Delete(int id)
+        public async Task<ConsultaClinicaDTO> Delete(int id)
         {
-            throw new NotImplementedException();
+            var consultaEncontrada = await _repository.GetById(id);
+            if (consultaEncontrada == null) return null;
+
+            await _repository.Delete(consultaEncontrada);
+
+            return Mapper.ConsultaClinicaMapToDTO(consultaEncontrada);
+        }
+
+        public async Task<IEnumerable<ConsultaClinicaDTO>> GetConsultasByIdMascota(int id)
+        {
+            var listado = await _repository.GetConsultasByIdMascota(id);
+            return listado.Select(Mapper.ConsultaClinicaMapToDTO);
         }
     }
 }
