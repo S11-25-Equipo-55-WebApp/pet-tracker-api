c3b04420125c86c415fad99cf14a2013bc68a3c5
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-12-07 13:07:47 -0300

    dto y mapper

---
+++ petTrackerApi/DTO/UnidadMedidaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace petTrackerApi.DTO
+{
+    public class UnidadMedidaDTO
+    {
+        public int UnidadMedidaId { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string Abreviatura { get; set; } = null!;
+    }
+}
